39f5509f66792186a113a74fb8b342fccd503b71
Olivier Benz <olivier.benz@b-data.ch>
2023-08-04 13:55:45 +0200

    Check shell scripts with ShellCheck

--- .devcontainer/r-base/scripts/usr/local/bin/postStartCommand.sh
+++ .devcontainer/r-base/scripts/usr/local/bin/postStartCommand.sh
@@ -6,16 +6,16 @@ set -e
 
 # Copy QGIS stuff from skeleton directory if home directory is bind mounted
 if [ "$(id -un)" == "root" ]; then
-  if [ ! -d /root/.local/share ] && [ $(command -v qgis) ]; then
+  if [ ! -d /root/.local/share ] && [ "$(command -v qgis)" ]; then
     cp -R /etc/skel/.local/share /root/.local;
   fi
 else
-  if [ ! -d "${HOME}/.local/share" ] && [ $(command -v qgis) ]; then
-    sudo cp -R /etc/skel/.local/share "${HOME}/.local";
-    sudo chown -R "$(id -u)":"$(id -g)" "${HOME}/.local/share";
+  if [ ! -d "$HOME/.local/share" ] && [ "$(command -v qgis)" ]; then
+    sudo cp -R /etc/skel/.local/share "$HOME/.local";
+    sudo chown -R "$(id -u)":"$(id -g)" "$HOME/.local/share";
   fi
 fi
 
 # Create R user package library
 RLU=$(Rscript -e "cat(Sys.getenv('R_LIBS_USER'))")
-mkdir -p ${RLU}
+mkdir -p "$RLU"
--- .devcontainer/scripts/usr/local/bin/onCreateCommand.sh
+++ .devcontainer/scripts/usr/local/bin/onCreateCommand.sh
@@ -4,15 +4,15 @@
 
 set -e
 
-mkdir -p "${HOME}/.local/bin"
-mkdir -p "${HOME}/projects"
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/projects"
 
 # Copy scripts from skeleton directory if home directory is bind mounted
-if [ ! -f "${HOME}/.local/bin/dockerSystemPrune.sh" ]; then
-  cp /etc/skel/.local/bin/dockerSystemPrune.sh "${HOME}/.local/bin";
+if [ ! -f "$HOME/.local/bin/dockerSystemPrune.sh" ]; then
+  cp /etc/skel/.local/bin/dockerSystemPrune.sh "$HOME/.local/bin";
 fi
-if [ ! -f "${HOME}/.local/bin/checkForUpdates.sh" ]; then
-  cp /etc/skel/.local/bin/checkForUpdates.sh "${HOME}/.local/bin";
+if [ ! -f "$HOME/.local/bin/checkForUpdates.sh" ]; then
+  cp /etc/skel/.local/bin/checkForUpdates.sh "$HOME/.local/bin";
 fi
 
 # Copy Bash-related files from root's backup directory
@@ -34,21 +34,21 @@ if [ "$(id -un)" == "root" ]; then
     cp /home/*/.zshrc /root;
   fi
 else
-  if [ ! -d "${HOME}/.oh-my-zsh" ]; then
-    sudo cp -R /root/.oh-my-zsh "${HOME}";
-    sudo chown -R "$(id -u)":"$(id -g)" "${HOME}/.oh-my-zsh";
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sudo cp -R /root/.oh-my-zsh "$HOME";
+    sudo chown -R "$(id -u)":"$(id -g)" "$HOME/.oh-my-zsh";
   fi
-  if [ ! -f "${HOME}/.zshrc" ]; then
-    sudo cp /root/.zshrc "${HOME}";
-    sudo chown "$(id -u)":"$(id -g)" "${HOME}/.zshrc";
+  if [ ! -f "$HOME/.zshrc" ]; then
+    sudo cp /root/.zshrc "$HOME";
+    sudo chown "$(id -u)":"$(id -g)" "$HOME/.zshrc";
   fi
 fi
 
 # Set PATH so it includes user's private bin if it exists
-if ! $(grep -q "user's private bin" $HOME/.zshrc); then
-  echo -e "\n# set PATH so it includes user's private bin if it exists\nif [ -d \"\$HOME/bin\" ] && [[ \"\$PATH\" != *\"\$HOME/bin\"* ]] ; then\n    PATH=\"\$HOME/bin:\$PATH\"\nfi" >> ${HOME}/.zshrc;
-  echo -e "\n# set PATH so it includes user's private bin if it exists\nif [ -d \"\$HOME/.local/bin\" ] && [[ \"\$PATH\" != *\"\$HOME/.local/bin\"* ]] ; then\n    PATH=\"\$HOME/.local/bin:\$PATH\"\nfi" >> ${HOME}/.zshrc;
+if ! grep -q "user's private bin" "$HOME/.zshrc"; then
+  echo -e "\n# set PATH so it includes user's private bin if it exists\nif [ -d \"\$HOME/bin\" ] && [[ \"\$PATH\" != *\"\$HOME/bin\"* ]] ; then\n    PATH=\"\$HOME/bin:\$PATH\"\nfi" >> "$HOME/.zshrc";
+  echo -e "\n# set PATH so it includes user's private bin if it exists\nif [ -d \"\$HOME/.local/bin\" ] && [[ \"\$PATH\" != *\"\$HOME/.local/bin\"* ]] ; then\n    PATH=\"\$HOME/.local/bin:\$PATH\"\nfi" >> "$HOME/.zshrc";
 fi
 
 # Remove old .zcompdump files
-rm -f ${HOME}/.zcompdump*
+rm -f "$HOME"/.zcompdump*
